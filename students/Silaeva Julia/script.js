16900bc80eef53ddb448f1a36e1ce2b76e64cb09
Julia Silaeva <silaichic@mail.ru>
2020-06-21 17:30:51 +0300

    добавила promise

--- students/Silaeva Julia/script.js
+++ students/Silaeva Julia/script.js
@@ -1,54 +1,86 @@
-class GoodsItem {
-    constructor(title, price) {
-        this.title = title;
-        this.price = price;
-    }
-    
-    render() {
-        return `<div class="goods-item"><h2>${this.title}</h2><p>${this.price}</p><button type="button" class="cart-button goods-button">В корзину</button></div>`;
-    }
+function makeGETRequest(url, callback) {
+	var xhr;
+
+	if (window.XMLHttpRequest) {
+		xhr = new XMLHttpRequest();
+	} else if (window.ActiveXObject) {
+		xhr = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4) {
+			callback(xhr.responseText);
+		}
+	}
+
+	xhr.open('GET', url, true);
+	xhr.send();
 }
-    
+
+const promise = new Promise((resolve, reject) => {
+	.then((callback) => {
+	console.log('processResult promise resolve', callback);
+})
+	.catch((error) => {
+		console.log('Promise rejected', error);
+	});
+
+});
+
+class GoodsItem {
+	constructor(title, price) {
+		this.title = title;
+		this.price = price;
+	}
+
+	render() {
+		return `<div class="goods-item"><h2>${this.title}</h2><p>${this.price}</p><button type="button" class="cart-button goods-button">В корзину</button></div>`;
+	}
+}
+
 class GoodsList {
-    constructor() {
-        this.goods = [];
-    }
-    
-    fetchGoods() {
-        this.goods = [
-            { title: 'Компьютер', price: 10000 },
-            { title: 'Мышь', price: 500 },
-            { title: 'Клавиатура', price: 1000 },
-            { title: 'Монитор', price: 7000 },
-        ];
-    }
-    
-    render() {
-        let goodsLayout = '';
-        this.goods.forEach(({ title, price }) => {
-            const item = new GoodsItem(title, price);
-            goodsLayout += item.render();
-        });
-        document.querySelector('.goods-list').innerHTML = goodsLayout;
-    }
-    
-    sum() {
-        let totalPrice = 0;
-        totalPrice +=goods.price
-    } //общая сумма товаров
+	constructor() {
+		this.goods = [];
+	}
+
+	fetchGoods() {
+		this.goods = [
+			{ title: 'Компьютер', price: 10000 },
+			{ title: 'Мышь', price: 500 },
+			{ title: 'Клавиатура', price: 1000 },
+			{ title: 'Монитор', price: 7000 },
+		];
+	}
+
+	render() {
+		let goodsLayout = '';
+		this.goods.forEach(({ title, price }) => {
+			const item = new GoodsItem(title, price);
+			goodsLayout += item.render();
+		});
+		document.querySelector('.goods-list').innerHTML = goodsLayout;
+	}
+
+	summary() {
+		let summ = 0;
+		this.goods.forEach(({ title, price }) => {
+			summ += price;
+		})
+		return summ;
+	}
 }
 
 class Basket {
-    constructor() {
-        this.selectedGoods = [] //выбранные товары
-    }
-    
-    addToBasket //добавить в корзину
-    
-    deleteFromBasket //удалить товар из корзины
-    
-    order //заказать
-    
+	constructor() {
+		this.selectedGoods = [] //выбранные товары
+	}
+
+	addToBasket //добавить в корзину
+
+	deleteFromBasket //удалить товар из корзины
+
+	order //заказать
+
 }
 
 const list = new GoodsList;
